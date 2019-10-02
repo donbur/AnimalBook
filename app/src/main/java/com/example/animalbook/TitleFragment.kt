03414a85d7289fb1b8175440640824727edcfc5c
donbur <donbur9141@gmail.com>
2019-10-02 12:27:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/animalbook/TitleFragment.kt
@@ -0,0 +1,32 @@
+package com.example.animalbook
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_title.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+
+//private const val ARG_PARAM1="param1"
+//private const val ARG_PARAM2="param2"
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_title, container, false)
+    }
+
+    fun setTitle(title:String){
+        titleText.text=title
+    }
+
+
+}
